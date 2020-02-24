8c0660f76c8453e7852ced4e32f518e6ac407732
Your Name <you@example.com>
2020-02-24 15:22:01 +0000

    final hungry bug

--- src/org/academiadecodigo/hackstreetboys/Snake.java
+++ src/org/academiadecodigo/hackstreetboys/Snake.java
@@ -3,27 +3,32 @@ package org.academiadecodigo.hackstreetboys;
 import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Ellipse;
 import org.academiadecodigo.simplegraphics.pictures.Picture;
-import org.academiadecodigo.hackstreetboys.Controls;
 
-public class Snake {
+import javax.swing.*;
+
+public class Snake extends Ellipse{
 
 
-    private double x = 20;
-    private double y = 20;
-    Ellipse snake = new Ellipse(x, y, 20, 20);
+
+    private boolean alive = false;
 
     private GameFrame rectangle;
     private int direction;
 
-
-
+    public Snake(double v, double v1, double v2, double v3,boolean isALive) {
+        super(v, v1, v2, v3);
+        setColor(Color.BLACK);
+        fill();
+        grow(0,0);
+        setAlive(isALive);
+    }
     // 0 - direction right, 1 - direction down, 2 - direction left, 3 - direction up
 
     //Ellipse snake1 = new Ellipse(50, 180, 30, 30);
     //Ellipse snake2 = new Ellipse(50, 210, 30, 30);
 
     public boolean insideRectangle() {
-        return (snake.getX() >= 0 && snake.getX() <= 590) && (snake.getY() >= 11 && snake.getY() <= 590);
+        return (getX() >= 0 && getX() <= 590) && (getY() >= 11 && getY() <= 590);
     }
 
     public void setUp() {
@@ -42,28 +47,30 @@ public class Snake {
         direction = 0;
     }
 
-    public void createSnake() {
-        snake.setColor(Color.BLUE);
-        snake.fill();
-
-
-        //snake1.setColor(Color.BLUE);
-        //snake1.fill();
-        //snake2.setColor(Color.BLUE);
-        //snake2.fill();
-    }
-
 
     private Picture snakedemo;
-    private Picture apple;
+    private Picture background;
+    private Picture banana;
+    private Picture gameover;
 
     public void spawn() {
         //snakedemo = new Picture(90, 90, "snakedemo.png");
         //snakedemo.grow(0, 20);
         //snakedemo.draw();
-        apple = new Picture(50, 50, "resources/apple.png");
-        apple.grow(10, 10);
-        apple.draw();
+
+
+
+        banana = new Picture(450,450, "resources/banana.png");
+        banana.grow(10,10);
+        banana.draw();
+
+    }
+
+    public void spawnGameOver() {
+        gameover = new Picture(100,200, "resources/gameover.png");
+        gameover.grow(20,20);
+        gameover.draw();
+
     }
 
     public void moveSnake() {
@@ -71,46 +78,57 @@ public class Snake {
 
 
             case 0:
-            if (snake.getX() >= 0 && snake.getX() <= 590) {
-                snake.translate(10, 0);
+            if (getX() >= 0 && getX() <= 590) {
+                translate(10, 0);
             }
             //snake1.translate(10,0);
             //snake2.translate(10,0);
-            System.out.println(snake.getX());
+            System.out.println(getX());
             break;
 
 
             case 2:
-                if (snake.getX() > 12 && snake.getX() < 590) {
-                    snake.translate(-10, 0);
+                if (getX() >= -1 && getX() < 590) {
+                    translate(-10, 0);
                     //snake1.translate(-10,0);
                     //snake2.translate(-10,0);
-                    System.out.println(snake.getX());
-                    break;
+                    System.out.println(getX());
                 }
+                    break;
+
 
 
 
             case 3:
-                if (snake.getY() >= 11 && snake.getY() <= 590) {
-                    snake.translate(0, -10);
-                    System.out.println(snake.getY());
-                    break;
+                if (getY() >= 11 && getY() <= 590) {
+                    translate(0, -10);
+                    System.out.println(getY());
                 }
+                    break;
+
                 //snake1.translate(0,-10);
                 //snake2.translate(0,-10);
 
 
 
             case 1:
-                if (snake.getY() >= 11 && snake.getY() <= 590) {
-                    snake.translate(0, 10);
-                    System.out.println(snake.getY());
-                    break;
+                if (getY() >= 11 && getY() <= 590) {
+                    translate(0, 10);
+                    System.out.println(getY());
                 }
+                    break;
+
                 //snake1.translate(0,10);
                 //snake2.translate(0,10);
             }
+        }
+
+        public boolean isAlive(){
+            return this.alive;
+        }
+
+        public void setAlive(boolean alive) {
+            this.alive = alive;
         }
     }
 
